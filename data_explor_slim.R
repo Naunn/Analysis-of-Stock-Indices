921102d19335fd52261d871b25b97e0f8751033c
Naunn <bartosz.m.lewandowski@gmail.com>
2023-05-25 19:54:52 +0200

    Literowka

--- data_explor_slim.R
+++ data_explor_slim.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+library(readxl)
 library(ggplot2)
 
 df_all_slim <-
@@ -12,42 +13,62 @@ df_corr_pearson <-
   # "odwybierzmy" kolumne dat
   select(!DATE) %>%
   # usunmy puste wartosci
-  drop_na() %>% 
+  drop_na() %>%
   cor(method = "pearson")
 
 # sprawdzmy korelacje - metoda Spearmana (metoda rang, tj. sprawdza czy wystepuja podobne wzrosty i spadki)
-df_corr_spearman <-  
+df_corr_spearman <-
   df_all_slim %>%
   # "odwybierzmy" kolumne dat
   select(!DATE) %>%
   # usunmy puste wartosci
-  drop_na() %>% 
+  drop_na() %>%
   cor(method = "spearman")
 
 # porownanie wizualne
 colnames(df_all_slim)
-# [1] "DATE"                     "^BTC"                     "^ETH"                    
-# [4] "^WIG"                     "^WIG20"                   "^DAX"                    
-# [7] "^CAC"                     "^UKX"                     "^SPX"                    
-# [10] "^HEX"                     "^UX"                      "^RTS"                    
-# [13] "^XU100"                   "^SHC"                     "intrate_Poland"          
-# [16] "intrate_Germany"          "intrate_France"           "intrate_United Kingdom"  
-# [19] "intrate_United States"    "intrate_Finland"          "intrate_Russia"          
-# [22] "inflation_Poland"         "inflation_Germany"        "inflation_France"        
-# [25] "inflation_United Kingdom" "inflation_United States"  "inflation_Finland"       
-# [28] "inflation_Russia"         "inflation_Türkiye"  
+# [1] "DATE"                     "^BTC"                     "^ETH"
+# [4] "^WIG"                     "^WIG20"                   "^DAX"
+# [7] "^CAC"                     "^UKX"                     "^SPX"
+# [10] "^HEX"                     "^UX"                      "^RTS"
+# [13] "^XU100"                   "^SHC"                     "intrate_Poland"
+# [16] "intrate_Germany"          "intrate_France"           "intrate_United Kingdom"
+# [19] "intrate_United States"    "intrate_Finland"          "intrate_Russia"
+# [22] "inflation_Poland"         "inflation_Germany"        "inflation_France"
+# [25] "inflation_United Kingdom" "inflation_United States"  "inflation_Finland"
+# [28] "inflation_Russia"         "inflation_Türkiye"
 
 ggplot(df_all_slim, aes(x = DATE)) +
   # The World Health Organization declared the COVID-19 outbreak
-  geom_vline(xintercept = as.numeric(as.Date("2020-01-30")), color = "red", linetype = "dashed") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2020-01-30")),
+    color = "red",
+    linetype = "dashed"
+  ) +
   # The World Health Organization declared the COVID-19 to be pandemic
-  geom_vline(xintercept = as.numeric(as.Date("2020-03-11")), color = "blue", linetype = "dotdash") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2020-03-11")),
+    color = "blue",
+    linetype = "dotdash"
+  ) +
   # Russian aggression against Ukraine
-  geom_vline(xintercept = as.numeric(as.Date("2022-02-24")), color = "black", linetype = "dashed") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2022-02-24")),
+    color = "black",
+    linetype = "dashed"
+  ) +
   # Bankruptcy of FTX
-  geom_vline(xintercept = as.numeric(as.Date("2022-11-07")), color = "red", linetype = "dashed") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2022-11-07")),
+    color = "red",
+    linetype = "dashed"
+  ) +
   # Colapse of Silicon Valley Bank
-  geom_vline(xintercept = as.numeric(as.Date("2023-03-10")), color = "blue", linetype = "dotted") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2023-03-10")),
+    color = "blue",
+    linetype = "dotted"
+  ) +
   labs(title = "Porównanie wskaźników",
        x = "DATE",
        y = NULL) +
@@ -66,15 +87,35 @@ df_all_slim_norm <-
 
 ggplot(df_all_slim_norm, aes(x = DATE)) +
   # The World Health Organization declared the COVID-19 outbreak
-  geom_vline(xintercept = as.numeric(as.Date("2020-01-30")), color = "red", linetype = "dashed") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2020-01-30")),
+    color = "red",
+    linetype = "dashed"
+  ) +
   # The World Health Organization declared the COVID-19 to be pandemic
-  geom_vline(xintercept = as.numeric(as.Date("2020-03-11")), color = "blue", linetype = "dotdash") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2020-03-11")),
+    color = "blue",
+    linetype = "dotdash"
+  ) +
   # Russian aggression against Ukraine
-  geom_vline(xintercept = as.numeric(as.Date("2022-02-24")), color = "black", linetype = "dashed") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2022-02-24")),
+    color = "black",
+    linetype = "dashed"
+  ) +
   # Bankruptcy of FTX
-  geom_vline(xintercept = as.numeric(as.Date("2022-11-07")), color = "red", linetype = "dashed") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2022-11-07")),
+    color = "red",
+    linetype = "dashed"
+  ) +
   # Colapse of Silicon Valley Bank
-  geom_vline(xintercept = as.numeric(as.Date("2023-03-10")), color = "blue", linetype = "dotted") +
+  geom_vline(
+    xintercept = as.numeric(as.Date("2023-03-10")),
+    color = "blue",
+    linetype = "dotted"
+  ) +
   labs(title = "Porównanie wskaźników (znormalizowanych do przedziału [0,1])",
        x = "DATE",
        y = NULL) +
